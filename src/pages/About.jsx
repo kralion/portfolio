063de4d2f8c3f90aa0d472d0dfcda468e02076dc
Joan <joan300501@gmail.com>
2023-10-25 12:55:35 -0700

    Resolve #6 Rough Notations

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import devBanner from "../assets/about-banner.gif";
 import Resume from "../assets/Resume.pdf";
 import { FileDown } from "lucide-react";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 function About() {
   return (
@@ -30,18 +31,31 @@ function About() {
           Who I am & What I do <span className="text-[#eb5e28]">?</span>
         </h3>
         <p className="text-gray-500 px-4  lg:px-0 lg:w-3/4 w-full ">
-          I'm Brayan, a passionate Software and AI Native Developer based in
-          Peru, South America. I thrive on challenges and am dedicated to
-          creating innovative solutions that make life easier for people. My
-          expertise lies in software development and AI, and I'm always eager to
-          take on new and exciting projects that push the boundaries of what's
-          possible and I'm excited to apply my skills to tackle the most
-          challenging tasks.
+          I'm Brayan, a passionate
+          <RoughNotation show={true} color="#F3CC4A" type="underline">
+            <span className="text-black px-2 font-mono font-semibold ">
+              Software
+            </span>
+          </RoughNotation>
+          and
+          <RoughNotation show={true} color="#F3CC4A" type="underline">
+            <span className="text-black px-2 font-mono font-semibold">
+              AI Native
+            </span>
+          </RoughNotation>
+          Developer based in Peru, South America. I thrive on challenges and I
+          am dedicated to creating innovative solutions that make life easier
+          for people. My expertise lies in software development and AI, and I'm
+          always eager to take on new and exciting projects that push the
+          boundaries of what's possible and I'm excited to apply my skills to
+          tackle the most challenging tasks.
           <br />
-          <span className="text-black bg-yellow-400 font-semibold">
-            Let's embark on this journey together, and together, we can turn
-            complex challenges into opportunities for growth and innovation.
-          </span>
+          <RoughNotation show={true} color="#F3CC4A" type="highlight">
+            <span className="text-black  font-semibold">
+              Let's embark on this journey together, and together, we can turn
+              complex challenges into opportunities for growth and innovation.
+            </span>
+          </RoughNotation>
         </p>
 
         <a className="icon" href={Resume} download="Resume.pdf">
